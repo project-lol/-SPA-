68aece826cd664e031496fbe2b2d822a4d1244f0
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-03-02 18:18:50 +0900

    feat : 카드 앞뒷면에 인사정보 나타내기

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,9 @@ class HomePage {
   render() {
     const title = new ContentTitle(this.$main, "Great PeoPle")
     title.render()
+
+    const cardView = new CardView(this.$main)
+    cardView.render()
   }
 }
 export default HomePage
