20de5a14bf23578136130469a2facccc04299714
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-03-03 12:19:10 +0900

    feat : 유효성 검증

--- answer/src/components/Form.js
+++ answer/src/components/Form.js
@@ -1,22 +1,39 @@
-export const input = (type, id, text) => {
+export const input = (type, id, text, required) => {
   const span = document.createElement("span")
   span.setAttribute("class", "form_elem")
 
   const input = document.createElement("input")
   input.setAttribute("type", type)
   input.setAttribute("id", id)
+  if (required) {
+    input.setAttribute("required", "")
+  }
+
+  if (id == "name") {
+    input.setAttribute("pattern", "^([가-힣]){2,4}$")
+    input.setAttribute("title", "2~4 글자의 한글만 입력이 가능합니다.")
+  }
+
+  const label = document.createElement("label")
+  label.setAttribute("for", id)
+  label.appendChild(document.createTextNode(text))
+  span.appendChild(label)
 
   span.appendChild(input)
   document.getElementById("grepp_form").appendChild(span)
 }
 
-export const select = (id, optValList, optTxtList) => {
+export const select = (id, optValList, optTxtList, required) => {
   const span = document.createElement("span")
   span.setAttribute("class", "form_elem")
 
   const select = document.createElement("select")
   select.setAttribute("id", id)
   select.setAttribute("name", id)
+
+  if (required) {
+    select.setAttribute("required", "")
+  }
 
   for (let i = 0; i < optValList.length; i++) {
     const option = document.createElement("option")
--- answer/src/components/SignupView.js
+++ answer/src/components/SignupView.js
@@ -12,9 +12,71 @@ class SignupView {
     form.setAttribute("id", "grepp_form")
     containerDiv.appendChild(form)
 
-    input("text", "name", "이름")
-    input("email", "email", "이메일")
-    input("text", "nickname", "닉네임")
+    form.addEventListener("submit", e => {
+      e.preventDefault()
+
+      const personalInfo = JSON.parse(localStorage.getItem("personalInfo"))
+
+      let idx = personalInfo.length
+      let nameVal = e.target.name.value
+      let emailVal = e.target.email.value
+      let nicknameVal = e.target.nickname.value
+
+      const submitInfo = {
+        name: nameVal,
+        email: emailVal,
+        nickname: nicknameVal,
+      }
+
+      localStorage.setItem("personalInfo", submitInfo)
+    })
+
+    input("text", "name", "이름", true)
+    input("email", "email", "이메일", true)
+    input("text", "nickname", "닉네임", true)
+
+    const roleValList = ["BE", "FE", "iOS", "Android", "Web"]
+    const roleTxtList = ["백엔드", "프론트엔드", "iOS", "Android", "Web"]
+    const mbtiValList = [
+      "INTJ",
+      "INTP",
+      "ENTJ",
+      "ENTP",
+      "INFJ",
+      "INFP",
+      "ENFJ",
+      "ENFP",
+      "ISTJ",
+      "ISFJ",
+      "ESTJ",
+      "ESFJ",
+      "ISTP",
+      "ISFP",
+      "ESTP",
+      "ESFP",
+    ]
+    const mbtiTxtList = [
+      "INTJ",
+      "INTP",
+      "ENTJ",
+      "ENTP",
+      "INFJ",
+      "INFP",
+      "ENFJ",
+      "ENFP",
+      "ISTJ",
+      "ISFJ",
+      "ESTJ",
+      "ESFJ",
+      "ISTP",
+      "ISFP",
+      "ESTP",
+      "ESFP",
+    ]
+    select("role", roleValList, rolTextList, true)
+    select("mbti", mbtiValList, mbtiTxtList, false)
+
+    button("submit", "등록")
   }
 }
 export default SignupView
